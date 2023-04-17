c3525456296b989ef733e0889657ae060ed935b0
Jesus D <130997254+jdrac@users.noreply.github.com>
2023-04-17 17:32:47 -0500

    pidra papel y tijera

---
+++ piedra.papael. y.tijera/js/codigo.js
@@ -0,0 +1,52 @@
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function eleccion (jugada){
+    let resultado = ""
+    if(jugada == 1){
+        resultado = "piedra"
+    }else if(jugada == 2) {
+        resultado = "papel"
+    }else if(jugada == 3){
+        resultado = "tijera"
+    }else {
+        resultado = "MAL ELEGIDO"
+    }
+    return resultado
+}
+// 1 es piedra, 2 es piedra, 3 es tijera
+let jugador = 0
+// let min = 1 
+// let max = 2  
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+
+while(triunfos < 3 && perdidas < 3) {
+    pc = aleatorio (1,3)
+    jugador = prompt("elige: 1 para pedra, 2 para papel, 3 para tijera")
+    // alert("elegiste " + jugador)
+
+    alert ("PC elige "+ eleccion(pc))
+    alert ("tu eligue " + eleccion(jugador))
+
+    // combate 
+    if(pc == jugador){
+        alert("empate")
+    }else if (jugador == 1 && pc == 3){
+        alert("ganaste")
+        triunfos = triunfos + 1
+    }else if (jugador == 2 && pc == 1){
+        alert("ganaste")
+        triunfos = triunfos + 1
+    }else if (jugador == 3 && pc == 2){
+        alert("ganaste")
+        triunfos = triunfos + 1
+    }else {
+        alert("perdiste")
+        perdidas = perdidas + 1
+    }
+}
+
+alert ("ganaste "+ triunfos + " veces, perdiste " + perdidas + " veces")
